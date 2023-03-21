bb024e6e0682f115bac9c6f34ecbb585f79b97e0
spirit <guohuiying0714@163.com>
2023-03-21 17:24:36 +0800

    20230321 【3-9员工管理】-【1.1通用工具栏的组件结构】

---
+++ src/components/PageTools/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-card class="page-tools">
+    <el-row type="flex" justify="space-between" align="middle">
+        <el-col>
+            <div v-if="showBefore" class="before">
+                <i class="el-icon-info" />
+                <slot name="before"></slot>
+            </div>
+        </el-col>
+        <el-col>
+            <el-row type="flex" justify="end">
+                <!-- type="flex" justify="end"要写在el-row标签上 -->
+                <!-- 比较设置:span="5"和不设置:span="5"的区别 -->
+                <el-col :span="5">
+                    <slot name="after" />
+                </el-col>
+            </el-row>
+        </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script>
+export default {
+    props:{
+        showBefore:{
+            type:Boolean,
+            default:true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    <PageTools>
+      <span slot="before" :showBefore="true">想见你</span>
+      <el-button slot="after">导入excel</el-button>
+    </PageTools>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+import PageTools from '@/components/PageTools/index.vue'
 export default {
   name: 'Dashboard',
   computed: {
     ...mapGetters([
       'name'
     ])
+  },
+  components: {
+    PageTools
   }
 }
 </script>
