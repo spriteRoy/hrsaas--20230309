be6bfcacc0ddcef1bc362e8ee8dd218d6dbece86
spirit <guohuiying0714@163.com>
2023-03-23 19:28:11 +0800

    【3.11权限设计和管理】-【3.3新增编辑权限的弹层】

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -14,7 +14,7 @@
         <el-table-column label="标识" prop="code" />
         <el-table-column label="描述" prop="description" />
         <el-table-column align="center" label="操作">
-          <template  slot-scope="{ row }">
+          <template slot-scope="{ row }">
             <el-button v-if="row.type === 1" type="text">添加</el-button>
             <el-button type="text">编辑</el-button>
             <el-button type="text">删除</el-button>
@@ -22,6 +22,35 @@
         </el-table-column>
       </el-table>
     </div>
+    <!-- 新增编辑弹层 -->
+    <!-- 放置一个弹层 用来编辑新增节点 -->
+    <el-dialog title="新增权限点" :visible="true">
+      <!-- 表单 -->
+      <el-form ref="perForm" :model="formData" label-width="120px">
+        <el-form-item label="权限名称" prop="name">
+          <el-input v-model="formData.name" style="width: 90%" />
+        </el-form-item>
+        <el-form-item label="权限标识" prop="code">
+          <el-input v-model="formData.code" style="width: 90%" />
+        </el-form-item>
+        <el-form-item label="权限描述">
+          <el-input v-model="formData.description" style="width: 90%" />
+        </el-form-item>
+        <el-form-item label="开启">
+          <el-switch
+            v-model="formData.enVisible"
+            active-value="1"
+            inactive-value="0"
+          />
+        </el-form-item>
+      </el-form>
+      <el-row slot="footer" type="flex" justify="center">
+        <el-col :span="6">
+          <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+          <el-button size="small" @click="btnCancel">取消</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -32,16 +61,26 @@ export default {
   data() {
     return {
       list: [],
+      formData: {
+        name: "", // 名称
+        code: "", // 标识
+        description: "", // 描述
+        type: "", // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
+        pid: "", // 因为做的是树 需要知道添加到哪个节点下了
+        enVisible: "0", // 开启
+      },
     };
   },
   created() {
     this.getPermissionList();
   },
   methods: {
-   async getPermissionList() {
+    async getPermissionList() {
       // 将数据转化成了 带chilren的树形结构
-      this.list = tranListToTreeData(await getPermissionList(), '0')
+      this.list = tranListToTreeData(await getPermissionList(), "0");
     },
+    btnOK(){},
+    btnCancel(){}
   },
 };
 </script>
