abee4c30a68468d16d4124e15cc520a2f599506f
spirit <guohuiying0714@163.com>
2023-03-23 19:07:01 +0800

    【3.11权限设计和管理】-【3.2获取权限数据并转化树形】

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -8,19 +8,40 @@
         </template>
       </page-tools>
       <!-- 表格 -->
-      <el-table border>
-        <el-table-column align="center" label="名称" />
-        <el-table-column align="center" label="标识" />
-        <el-table-column align="center" label="描述" />
+      <!-- id为对象的唯一标识 -->
+      <el-table :data="list" border="" row-key="id">
+        <el-table-column label="名称" prop="name" />
+        <el-table-column label="标识" prop="code" />
+        <el-table-column label="描述" prop="description" />
         <el-table-column align="center" label="操作">
-          <template>
-            <el-button type="text">添加</el-button>
+          <template  slot-scope="{ row }">
+            <el-button v-if="row.type === 1" type="text">添加</el-button>
             <el-button type="text">编辑</el-button>
             <el-button type="text">删除</el-button>
           </template>
         </el-table-column>
-
       </el-table>
     </div>
   </div>
 </template>
+<script>
+import { getPermissionList } from "@/api/permission";
+// 注意：tranListToTreeData而不是transListToTreeData
+import { tranListToTreeData } from "@/utils/index";
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    this.getPermissionList();
+  },
+  methods: {
+   async getPermissionList() {
+      // 将数据转化成了 带chilren的树形结构
+      this.list = tranListToTreeData(await getPermissionList(), '0')
+    },
+  },
+};
+</script>
